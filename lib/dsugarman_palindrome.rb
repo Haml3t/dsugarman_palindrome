d6ecbb4e22137088fa81bbe3a77bb9d3fcb63767
David Sugarman <d.m.sugarman@gmail.com>
2018-12-03 15:17:04 -0400

    Initialize repository

---
+++ lib/dsugarman_palindrome.rb
@@ -0,0 +1,6 @@
+require "dsugarman_palindrome/version"
+
+module DsugarmanPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dsugarman_palindrome/version.rb
@@ -0,0 +1,3 @@
+module DsugarmanPalindrome
+  VERSION = "0.1.0"
+end
